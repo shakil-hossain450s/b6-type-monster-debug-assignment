a7c9fb063564048d9cb374255d473df830a20d73
shakil <shakil.hossain450s@gmail.com>
2022-09-15 10:03:24 +0600

    add blog button and blog file

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${Math.round(timeTaken)}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
